f6097c4903de0b516c6519196c5852c6b6226ab9
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-09 06:49:44 +0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,25 +1,18 @@
-function logoutUser(){
+function logoutUser() {
 
+const confirmLogout =
+    window.confirm("Are you sure you want to logout?");
 
-let confirmLogout =
-confirm("Are you sure you want to logout?");
-
-
-
-if(confirmLogout){
-
-
-localStorage.removeItem(
-"cashnovaCurrentUser"
-);
-
-
-
-window.location.href =
-"login.html";
-
-
+// CANCEL → DO NOTHING
+if (!confirmLogout) {
+    return;
 }
 
+// CONFIRM → CLEAR LOGIN SESSION
+localStorage.removeItem("cashnovaCurrentUser");
+localStorage.removeItem("cashnovaUserId");
+
+// GO TO LOGIN PAGE
+window.location.href = "login.html";
 
 }
