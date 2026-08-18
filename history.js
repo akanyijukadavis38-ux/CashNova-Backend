2045f35c0ed6a4973448d81e95e8f131d6335fb7
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-18 15:36:37 +0300

    Update history.js

--- history.js
+++ history.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         const response =
             await fetch(
-                "https://cashnova-backend-89lg.onrender.com/api/users/history/" +
+                "https://cashnova-backend-production-2404.up.railway.app/api/users/history/" +
                 userId
             );
 
