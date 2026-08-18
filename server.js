6f274419038f07342fa99881faa1fbd117d54850
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-18 14:40:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,25 +7,22 @@ const withdrawalRoutes = require("./withdrawalRoutes");
 const userRoutes = require("./userRoutes");
 const adminRoutes = require("./adminRoutes");
 
-
 const app = express();
+
 // Allow frontend connection
 app.use(cors());
-
 
 // Read JSON data
 app.use(express.json());
 
-
 // Connect database
 connectDB();
-
-
 
 app.use("/api/users", userRoutes);
 app.use("/api/deposits", depositRoutes);
 app.use("/api/withdrawals", withdrawalRoutes);
 app.use("/api/admin", adminRoutes);
+
 // Test route
 app.get("/", function(req, res){
 
@@ -33,11 +30,9 @@ app.get("/", function(req, res){
 
 });
 
-
 const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, function(){
+app.listen(PORT, "0.0.0.0", function(){
 
     console.log(
         "CashNova server running on port " + PORT
